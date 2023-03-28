9e7f5b88a11c89c6639c3db176139f2cdbc9bc4c
ShravanEssence <shravankumar.essence@gmail.com>
2023-03-28 18:11:25 +0530

    Cleared doubt regarding private

--- programs/Access_modifiers.rb
+++ programs/Access_modifiers.rb
@@ -1,17 +1,17 @@
 #Usage with public, private and protected (difference)
-
+#if you use only private keyword then the private method can be accessed from the subclass which is wrong so to prevent that you can see the below program
+#In this example you won't be able to access the private class method directly from the subclass.
+#If you want to access it indirectly then you can create public class method and in that method you can call the private class method.
 class Animal
 	def name(name)
 		puts "Name is: #{name}"
-		age
 	end
-	#Private methods can't be called from the outside of the class.
-	#they can only be called from the other methods of the same class
-	private
-	def age
-		puts "His age is 3"
+	private_class_method def self.age
+    puts "His age is 3"
+  end
+  public_class_method def self.get_age
+		self.age
 	end
-	#same as private it can't be called from the outside of the class.
 	protected
 	def gender
 		puts "He is Male"
@@ -22,18 +22,18 @@ class Dog < Animal
 	def name(name)
 		super
 	end
+	#below commented code gives error as the private class method won't be called.
+	#def genderr
+	#	Animal.age
+	#end
 	def genderr
-		age
+		Animal.get_age
 	end
 end
-=begin
-class hello
-	def genderr
-		Dog::gender
-	end
-end
-=end
-Dog.new.name("Max")
-Dog.new.genderr
 
-Dog.age
+d = Dog.new
+Animal.new.name("Tommy")
+Animal.get_age
+d.name("Max")
+Dog.new.genderr
+d.genderr
